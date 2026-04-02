1eaa61226c9969e25ebb94b1b9839d2c7312694f
Matthew <matthew.gilez@gmail.com>
2026-04-02 00:35:07 +0000

    feat/cdk-simple-s3-example

---
+++ s3/iac/cdk/lib/cdk-stack.ts
@@ -0,0 +1,19 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+import { RemovalPolicy } from 'aws-cdk-lib';
+
+export class CdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    new s3.Bucket(this, 'DemoBucket', {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      versioned: false,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,   // empties the bucket before deletion
+    });
+  }
+}
