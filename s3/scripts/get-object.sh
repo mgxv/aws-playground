35387bc0b17d8b7066256b62e96a47332fa22d77
Matthew Giles <matthew.gilez@gmail.com>
2026-04-03 23:30:07 +0000

    feat: add script to download S3 object to current directory

---
+++ s3/scripts/get-object.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "Usage: $0 <bucket-name> <object-key>"
+  exit 1
+fi
+
+# Disable auto-prompt
+export AWS_CLI_AUTO_PROMPT=off
+
+ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+echo "Account ID: $ACCOUNT_ID"
+
+REGION=$(aws configure get region)
+echo "Region: $REGION"
+
+BUCKET_NAME="${1}-${ACCOUNT_ID}-${REGION}-an"
+OBJECT_KEY="$2"
+DEST="./$(basename "$OBJECT_KEY")"
+echo "Downloading s3://$BUCKET_NAME/$OBJECT_KEY to $DEST"
+
+aws s3api get-object --bucket "$BUCKET_NAME" --key "$OBJECT_KEY" "$DEST"
+
+# Re-enable auto-prompt
+export AWS_CLI_AUTO_PROMPT=on
+
+echo "Done!"
